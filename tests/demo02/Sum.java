05a7f0beb005e7977a626dde18f18df60dd50edf
Jose Gavilan <Jose Gavilan>
2016-10-21 13:40:16 +0200

    Refactorización final

---
+++ tests/demo02/Sum.java
@@ -0,0 +1,7 @@
+package demo02;
+
+public class Sum {
+	public static int sum(int a, int b){
+		return a+b;
+	}
+}
--- tests/demo02/TestSum.java
+++ tests/demo02/TestSum.java
@@ -8,21 +8,19 @@ public class TestSum {
 	
 	@Test
 	public void testSum2Number(){
-		assertEquals(6,sum(2,4));
+		assertEquals(6,Sum.sum(2,4));
 	}
 	@Test
 	public void testSumWithZero(){
-		assertEquals(4, sum(0,4));
-		assertEquals(4,sum(4,0));
+		assertEquals(4, Sum.sum(0,4));
+		assertEquals(4,Sum.sum(4,0));
 	}
 	@Test
 	public void testSumWithNegativos(){
-		assertEquals(10, sum(12,-2));
-		assertEquals(-7,sum(-4,-3));
+		assertEquals(10, Sum.sum(12,-2));
+		assertEquals(-7,Sum.sum(-4,-3));
 	}
 	
-	int sum(int a, int b){
-		return a+b;
-	}
+	
 
 }
