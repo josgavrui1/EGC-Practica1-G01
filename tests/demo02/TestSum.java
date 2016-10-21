3b47236bede3a58bb943a0856fecda034c63bdfe
Jose Gavilan <Jose Gavilan>
2016-10-21 13:22:54 +0200

    Soporte para ceros

--- tests/demo02/TestSum.java
+++ tests/demo02/TestSum.java
@@ -8,7 +8,16 @@ public class TestSum {
 	
 	@Test
 	public void testSum2Number(){
-		assertEquals(6,2+4);
+		assertEquals(6,sum(2,4));
+	}
+	@Test
+	public void testSumWithZero(){
+		assertEquals(4, sum(0,4));
+		assertEquals(4,sum(4,0));
+	}
+	
+	int sum(int a, int b){
+		return a+b;
 	}
 
 }
